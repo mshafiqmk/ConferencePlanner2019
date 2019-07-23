30a4842138549111d73bdd0b3990ff43bfb02a0b
Muhammad Shafiq <Muhammad.shafiq@bentely.com>
2019-07-23 18:12:52 +0500

    Add project files.

---
+++ BackEnd/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Models
+    {
+    public class ApplicationDbContext : DbContext
+        {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+            {
+            }
+
+        public DbSet<Speaker> Speakers
+            {
+            get; set;
+            }
+        }
+    }
---
+++ BackEnd/Models/Speaker.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd.Models
+    {
+    public class Speaker
+        {
+        public int Id
+            {
+            get; set;
+            }
+
+        [Required]
+        [StringLength(200)]
+        public string Name
+            {
+            get; set;
+            }
+        [Required]
+        [StringLength(200)]
+        public string Bio
+            {
+            get; set;
+            }
+        [Required]
+        [StringLength(200)]
+        public virtual string Website
+            {
+            get; set;
+            }
+        }
+    }

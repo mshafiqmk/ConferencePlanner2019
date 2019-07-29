406dc2508c8ae01d521003fa2048d7dcbd2a35f1
Muhammad Shafiq <Muhammad.shafiq@bentely.com>
2019-07-29 14:22:51 +0500

    Added Refactor EF migration

--- BackEnd/Data/Speaker.cs
+++ BackEnd/Data/Speaker.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BackEnd.Data
     {
     public class Speaker : ConferenceDTO.Speaker
         {
-
+        public virtual ICollection<SessionSpeaker> SessionSpeakers { get; set; } = new List<SessionSpeaker> ();
         }
     }

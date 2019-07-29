516fad9311b039214cc1eaea0a4d987f499688ab
Muhammad Shafiq <Muhammad.shafiq@bentely.com>
2019-07-29 14:24:41 +0500

    Added model in data name-space of backend project

---
+++ BackEnd/Data/Attendee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConferenceDTO;
+
+namespace BackEnd.Data
+    {
+    public class Attendee : ConferenceDTO.Attendee
+        {
+        public virtual ICollection<ConferenceAttendee> ConferenceAttendees
+            {
+            get; set;
+            }
+
+        public virtual ICollection<Session> Sessions
+            {
+            get; set;
+            }
+        }
+    }
---
+++ BackEnd/Data/Conference.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConferenceDTO;
+
+namespace BackEnd.Data
+    {
+    public class Conference : ConferenceDTO.Conference
+        {
+        public virtual ICollection<Track> Tracks
+            {
+            get; set;
+            }
+
+        public virtual ICollection<Speaker> Speakers
+            {
+            get; set;
+            }
+
+        public virtual ICollection<Session> Sessions
+            {
+            get; set;
+            }
+
+        public virtual ICollection<ConferenceAttendee> ConferenceAttendees
+            {
+            get; set;
+            }
+        }
+    }
